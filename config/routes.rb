d227ab524db0db6969602504127642b5231ad6fa
MariDavila <maria.davila@osgcorp.com>
2018-01-08 16:14:30 -0500

    add map to dashboard centered on metropolitan area

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
 
-  resources :address
+  resources :addresses
 
   root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
