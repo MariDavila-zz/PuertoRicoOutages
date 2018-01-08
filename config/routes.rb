3b53a0636fa8eec3ef31cc8153dd4b83af438197
MariDavila <maria.davila@osgcorp.com>
2018-01-08 15:31:24 -0500

    add project to github. Add dashboard route, and resources :address

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
+  resources :address
+
+  root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
